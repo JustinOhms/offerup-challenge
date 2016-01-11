9d9e4229ecb0c20d6b5b54b58eeefb486e9a4202
JustinOhms <justinohms@gmail.com>
2016-01-10 16:28:54 -0800

    Adds guard for missing item and city
    fix failing nothing nowhere should return 404 test

--- server/api.js
+++ server/api.js
@@ -4,6 +4,21 @@ exports.read = function(req, res, next){
 		var other = req.query.other;
 	
 		
+		//Guards
+		//if we neither city or item were provided then send the 404 error
+		if(item == undefined && city == undefined){
+			res.send({
+				"status":404,
+				"content":{
+					"message":"Not found"
+				}
+			});
+			next();
+		}
+		
+		
+		
+		
 		//for now just echo the response
 		res.contentType = 'json';
 		res.send({
